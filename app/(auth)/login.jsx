ff703a2fd77a93a8555c5470688b2cf9baf0df20
Damileee <olasojudamilare6@gmail.com>
2025-05-19 18:58:33 +0100

    refactor: restructure app layout and authentication flow; add login and registration screens

---
+++ app/(auth)/login.jsx
@@ -0,0 +1,80 @@
+import { StyleSheet, Text } from 'react-native'
+import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
+import { useState } from 'react'
+
+import ThemedView from '../../components/ThemedView'
+import ThemedText from '../../components/ThemedText'
+import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
+import ThemedTextInput from '../../components/ThemedTextInput'
+
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = () => {
+    console.log("Login form submitted", email, password);
+  }
+
+  return (
+    <ThemedView style={styles.container}>
+      
+      <Spacer />
+      <ThemedText title={true} style={styles.title}>
+        Login to Your Account
+      </ThemedText>
+
+      <ThemedTextInput 
+        style={{ width: "80%", marginBottom: 20 }}
+        placeholder='Email' 
+        keyboardType='email-address'
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <ThemedTextInput
+        style={{ width: "80%", marginBottom: 20 }}
+        placeholder='Password' 
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: '#f2f2f2' }}>Login</Text>
+      </ThemedButton>
+
+      <Spacer height={100} />
+      <Link href="/register" replace>
+        <ThemedText style={{ textAlign: "center" }}>
+          Register instead
+        </ThemedText>
+      </Link>
+
+    </ThemedView>
+  )
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    marginBottom: 30
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
+  },
+})
---
+++ app/index.jsx
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+// themed components
+import ThemedView from '../components/ThemedView';
+import ThemedLogo from '../components/ThemedLogo';
+import ThemedText from '../components/ThemedText';
+import Spacer from '../components/Spacer';
+
+const Home = () => {
+    return (
+        <ThemedView style={styles.container}>
+            <ThemedLogo />
+            <Spacer />
+
+            <ThemedText style={styles.title} title={true}>
+                The Number 1
+            </ThemedText>
+
+            <Spacer height={10}/>
+            <ThemedText>Reading List App</ThemedText>
+            <Spacer />
+
+            <Link href="/login" style={styles.link}>
+                <ThemedText>Login Page</ThemedText>
+            </Link>
+            <Link href="/register" style={styles.link}>
+                <ThemedText>Register Page</ThemedText>
+            </Link>
+            <Link href="/profile" style={styles.link}>
+                <ThemedText>Profile Page</ThemedText>
+            </Link>
+        </ThemedView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    link: {
+        marginVertical: 10,
+        borderBottomWidth: 1,
+    }
+});
+
+export default Home;
