dd9d4edb30b90441df0f225a3f40ff8899d2bc69
Damileee <olasojudamilare6@gmail.com>
2025-05-21 22:18:33 +0100

    feat: create Transfer component with basic layout and styling

---
+++ app/(tabs)/transfer.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from 'react-native'
+
+const Transfer = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
+}
+
+export default Transfer
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+  },
+})
