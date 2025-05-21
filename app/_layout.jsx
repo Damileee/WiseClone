937823269e01b6d3fbbc4407aff7e3dba055ce03
Damileee <olasojudamilare6@gmail.com>
2025-05-21 22:20:03 +0100

    feat: simplify RootLayout component by removing unused imports and restructuring screen logic

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,26 +1,16 @@
-import { Stack } from "expo-router"
-import { Colors } from "../constants/Colors"
-import { useColorScheme } from "react-native"
-import { StatusBar } from "expo-status-bar"
+import { Stack } from 'expo-router';
+import { useState } from 'react';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
-  const theme = Colors[colorScheme] ?? Colors.light
+  const [isLoggedIn] = useState(false); // Replace with real auth later
 
   return (
-    <>
-      <StatusBar value="auto" />
-      <Stack screenOptions={{
-        headerStyle: { backgroundColor: theme.navBackground },
-        headerTintColor: theme.title,
-      }}>
-        {/* Groups */}
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-
-        {/* Individual Screens */}
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-      </Stack>
-    </>
-  )
+    <Stack screenOptions={{ headerShown: false }}>
+      {!isLoggedIn ? (
+        <Stack.Screen name="(auth)" />
+      ) : (
+        <Stack.Screen name="(tabs)" />
+      )}
+    </Stack>
+  );
 }
